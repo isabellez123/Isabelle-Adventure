da1996a0db83a10758e725076a56d7437100a83a
isabellez123 <168394780+isabellez123@users.noreply.github.com>
2024-05-08 08:44:07 -0400

    Add files via upload
    I watched this video https://www.youtube.com/watch?v=AX8YU2hLBUg to help me create a scrolling background, making my background move only when my player moves. I also set up my code so that it changes backgrounds when the player reaches a certain point. Code doesn't work as of right now however I plan on polishing it next class.

---
+++ bird.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+class Bird:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image_list = ["bird-up.png", "bird-down.png"]
+        self.image = pygame.image.load(self.image_list[0])
+        self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = 4
+        self.up = True
+
+    def rescale_image(self, image):
+        self.image_size = self.image.get_size()
+        scale_size = (self.image_size[0] * .7, self.image_size[1] * .7)
+        self.image = pygame.transform.scale(self.image, scale_size)
+
+    def move_bird(self, direction):
+        if direction == "right":
+            self.x = self.x + self.delta
+        if direction == "left":
+            self.x = self.x - self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+    def switch_image(self):
+        image_number = 0
+        if not self.up:
+            image_number = 1
+        self.image = pygame.image.load(self.image_list[image_number])
+        self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
+        self.up = not self.up
+
--- main.py
+++ main.py
@@ -1,196 +1,128 @@
 import pygame
 import random
-import time
-from fox import Fox
-from fox_2 import Fox2
-from coin import Coin
-from redcoin import Red_coin
-from bomb import Bomb
+from balloon import Balloon
 
-start_time = time.time()
-red_coin_start = time.time()
-red_coin_countdown = -1
 # set up pygame modules
 pygame.init()
 pygame.font.init()
-my_font = pygame.font.SysFont('Arial', 15)
-pygame.display.set_caption("Coin Collector!")
-# set up variables for the display
-SCREEN_HEIGHT = 370
-SCREEN_WIDTH = 530
-size = (SCREEN_WIDTH, SCREEN_HEIGHT)
-screen = pygame.display.set_mode(size)
+my_font = pygame.font.SysFont('Arial', 28)
+pygame.display.set_caption("Balloon Flight!")
 
-name = "Collect coins as fast as you can!"
-message = "Collision not detected"
-r = 50
-g = 0
-b = 100
+FPS = 500
+
+start_message = "WELCOME TO BALLOON FIGHT."
+instruct_1 = "Click the mouse to move the balloon up and down"
+instruct_2 = "Dodge the Flying birds. Click to start!"
+end_message = "You suck. Click to restart"
+
+
+# set up variables for the display
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+BALLOON_START_X = 20
+BALLOON_START_Y = 430
+
+bg = pygame.image.load("background.png")
+bg_2 = pygame.image.load("")
+house = pygame.image.load("house.png")
+tree = pygame.image.load("tree.png")
+
+display_end_message = my_font.render(end_message, True, (255, 0, 0))
+display_start_message = my_font.render(start_message, True, (255, 0, 0))
+
+my_font = pygame.font.SysFont('Arial', 15)
+display_instruct_1 = my_font.render(instruct_1, True, (255, 255, 255))
+display_instruct_2 = my_font.render(instruct_2, True, (255, 255, 255))
+
+balloon = Balloon(BALLOON_START_X, BALLOON_START_Y)
+
+INITIAL_HOUSE_X = 900
+INITIAL_TREE_X = 1100
+house_x = INITIAL_HOUSE_X
+tree_x = INITIAL_TREE_X
 
 # render the text for later
-display_name = my_font.render(name, True, (255, 255, 255))
-display_message = my_font.render(message, True, (255, 255, 255))
-
-#initializing the objects
-
-f = Fox(40, 60)
-c = Coin(200, 85)
-rc = Red_coin(200, 85)
-f_2 = Fox2(40, 40)
-# b = Bomb(200, 300)
-
-score = 0
-start = False
-game_end = False
-lose = False
-red_coin_timer = 0
-top_score = False
-red_coin = False
-bomb = 0
 
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
-run = True
 
-file = open("highscore", "r")
-high_score = file.readline().strip()
-print(int(high_score))
+run = True
+game_over = False
+start_screen = True
 
 # -------- Main Program Loop -----------
+clock = pygame.time.Clock()
+frame = 0
+points = 0
+direction = "down"
+
 while run:
-    instruct_1 = my_font.render("Use ASWD to move", True, (255, 255, 255))
-    instruct_2 = my_font.render("You have 10 seconds to collect 10 coins", True, (255, 255, 255))
-    instruct_3 = my_font.render("Click anywhere on the screen to begin!", True, (255, 255, 255))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        elif event.type == pygame.MOUSEBUTTONUP and not start:  # Check for mouse click to start the game
-            start = True
-
-    if not start:
-        screen.fill((r, g, b))
-        screen.blit(f.image, f.rect)
-        screen.blit(instruct_1, (150, 60))
-        screen.blit(instruct_2, (150, 110))
-        screen.blit(instruct_3, (150, 160))
-        pygame.display.update()
-
-    elif not game_end:
-        countdown = 12
-        elapsed_time = time.time() - start_time
-        timer = countdown - elapsed_time
-        timer = round(timer, 2)
-        render_elapsed_time = my_font.render("Time: " + str(timer), True, (255, 255, 255))
-
-        keys = pygame.key.get_pressed()  # checking pressed keys
-        if keys[pygame.K_d]:
-            f.move_direction("right")
-        if keys[pygame.K_a]:
-            f.move_direction("left")
-        if keys[pygame.K_w]:
-            f.move_direction("up")
-        if keys[pygame.K_s]:
-            f.move_direction("down")
-
-        # blue fox
-
-        if keys[pygame.K_RIGHT]:
-            f_2.move_direction("right")
-        if keys[pygame.K_LEFT]:
-            f_2.move_direction("left")
-        if keys[pygame.K_UP]:
-            f_2.move_direction("up")
-        if keys[pygame.K_DOWN]:
-            f_2.move_direction("down")
-
-
-        # collision
-        if f.rect.colliderect(c.rect) or f_2.rect.colliderect(c.rect):
-            message = "Collision detected"
-            display_message = my_font.render(message, True, (255, 255, 255))
-            x = random.randint(1, 400)
-            y = random.randint(1, 300)
-            c.set_location(x, y)
-            score = score + 10
-            render_score = my_font.render(str(score), True, (255, 255, 255))
-        else:
-            message = "Collision not detected"
-            display_message = my_font.render(message, True, (255, 255, 255))
-
-        # if f.rect.colliderect(b.rect):
-        #     score = score - 10
-        #     bomb = bomb + 1
-        #     x_new = random.randint(445)
-        #     y_new = random.randint(0, 280)
-        #     b.set.location(x_new, y_new)
-        #     display_bomb_count = my_font.render(("Bomb count is: ") + str(bomb), True, (255,255,255))
-        #     render_score = my_font.render(str(score), True, (255, 255, 255))
-        #
-        # if bomb == 5:
-        #     game_end = True
-
-        if red_coin_countdown < 0:
-            if random.randint(1, 30) == 1:
-                x = random.randint(1, 400)
-                y = random.randint(1, 300)
-                rc.set_location(x, y)
-                red_coin_start = time.time()
-                red_coin_countdown = random.randint(3, 10)  # Randomize time for red coin appearance
-
-        red_coin_elapsed_time = time.time() - red_coin_start
-        red_coin_countdown = red_coin_countdown - red_coin_elapsed_time
-
-        if f.rect.colliderect(rc.rect) or f_2.rect.colliderect(rc.rect):
-            message = "Collision with red coin detected!"
-            score = score + 25
-            red_coin = False
-            display_message = my_font.render(message, True, (255, 255, 255))
-            rc.set_location(-100, -100)  # Move the red coin off-screen
-
-
-        # --- Main event loop
+    # -- start screen
+    if start_screen:
         for event in pygame.event.get():  # User did something
             if event.type == pygame.QUIT:  # If user clicked close
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                start_screen = False
 
-        if timer <= 0 :
-            game_end = True
-
-            end_message = my_font.render("Game over, you win!", True, (255, 255, 255))
-            screen.fill((r, g, b))
-            screen.blit(end_message, (200, 150))
-            pygame.display.update()
-
-        else:
-            if red_coin_countdown < 0:
-                red_coin_countdown -= 1
-
-            render_score = my_font.render("Score: " + str(score), True, (255, 255, 255))
-            screen.fill((r, g, b))
-            screen.blit(display_name, (0, 0))
-            screen.blit(render_score, (0, 100))
-            screen.blit(display_message, (0, 15))
-            screen.blit(render_elapsed_time, (0, 150))
-            screen.blit(rc.image, rc.rect)
-            screen.blit(f_2.image, f_2.rect)
-            screen.blit(f.image, f.rect)
-            screen.blit(c.image, c.rect)
-            pygame.display.update()
-
-    # game ends
-    if game_end == True:
-        # high score stuff
-
-        highscore_display = my_font.render("HIGHSCORE: " + str(high_score), True, (255, 255, 255))
-
-        score_display = my_font.render(" YOUR SCORE: " + str(score), True, (255, 255, 255))
-        screen.fill((r, g, b))
-        screen.blit(highscore_display, (100, 100))
-        screen.blit(score_display, (200, 200))
+        # -- start screen here
+        screen.blit(bg, (0, 0))
+        screen.blit(display_start_message, (200, 200))
+        screen.blit(display_instruct_1, (200, 250))
+        screen.blit(display_instruct_2, (200, 300))
         pygame.display.update()
-# Once we have exited the main program loop we can stop the game engine:
-if int(high_score) < score:
-    n = open("highscore", "w")
-    n.write(str(score))
 
+    # -- game
+    if not game_over and not start_screen:
+
+        if house_x < -250:
+            house_x = INITIAL_HOUSE_X
+
+        if tree_x < -250:
+            tree_x = INITIAL_TREE_X
+
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                run = False
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_d]:
+                balloon.move_balloon("right")
+            if keys[pygame.K_a]:
+                balloon.move_balloon("left")
+            if Balloon(100, 430):
+                house_x = house_x - 30
+                tree_x = tree_x - 30
+
+            screen.blit(bg, (0, 0))
+            screen.blit(house, (house_x, 360))
+            screen.blit(tree, (tree_x, 360))
+            screen.blit(balloon.image, balloon.rect)
+            pygame.display.update()
+
+            if Balloon(500,430):
+                screen.blit(bg_2, (0, 0))
+
+        balloon.move_balloon(direction)
+
+        frame += 2
+
+    # -- end screen
+    if game_over:
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                points = 0
+                balloon.x = BALLOON_START_X
+                balloon.y = BALLOON_START_Y
+                house_x = INITIAL_HOUSE_X
+                tree_x = INITIAL_TREE_X
+                game_over = False
+
+        # -- end screen here
+        screen.blit(bg, (0, 0))
+        screen.blit(display_end_message, (300, 300))
+        pygame.display.update()
+
+# Once we have exited the main program loop we can stop the game engine:
 pygame.quit()
