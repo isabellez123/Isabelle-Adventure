cd9b0e0473de6db6d7fdc811b05a0e91cc30e428
isabellez123 <168394780+isabellez123@users.noreply.github.com>
2024-05-17 08:43:02 -0400

    Add files via upload
    I continued to fix my glitch today and made some slight progress with Mr Das. The screen isn't glitching anymore however the two backgrounds still continue to overlap over each other. In the meantime, I added my second player and started to draw more characters/objects that will be included.

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from user_player import Player
 pygame.init()
 pygame.font.init()
 my_font = pygame.font.SysFont('Arial', 28)
-pygame.display.set_caption("Balloon Flight!")
 
 FPS = 60
 
-start_message = "Welcome to Isabelle's Awesome Adventure."
+start_message = "Welcome to Isabelle's Adventure."
 instruct_1 = "Use controls WASD for movement"
 instruct_2 = "Make bold decisions and interact with characters. Click to start"
 end_message = "Congrats! You've completed the journey."
@@ -27,6 +26,7 @@ bg_1 = pygame.image.load("background.png")
 bg_2 = pygame.image.load("background_2.png")
 house = pygame.image.load("house.png")
 tree = pygame.image.load("tree.png")
+player_2 = pygame.image.load("player_2.png")
 
 #rendering
 display_end_message = my_font.render(end_message, True, (255, 0, 0))
@@ -42,6 +42,10 @@ INITIAL_HOUSE_X = 900
 INITIAL_TREE_X = 1100
 house_x = INITIAL_HOUSE_X
 tree_x = INITIAL_TREE_X
+PLAYER_2_START_X = 60
+PLAYER_2_START_Y = 440
+player_2_x = PLAYER_2_START_X
+player_2_y = PLAYER_2_START_Y
 
 # render the text for later
 
@@ -63,7 +67,6 @@ while run:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 start_screen = False
 
-        # -- start screen here
         screen.blit(bg_1, (0, 0))
         screen.blit(display_start_message, (200, 200))
         screen.blit(display_instruct_1, (200, 250))
@@ -73,24 +76,50 @@ while run:
     # -- game
     if not game_over and not start_screen:
 
-        if house_x < -250:
-            house_x = INITIAL_HOUSE_X
-
-        if tree_x < -250:
-            tree_x = INITIAL_TREE_X
-
         for event in pygame.event.get():  # User did something
             if event.type == pygame.QUIT:  # If user clicked close
                 run = False
             keys = pygame.key.get_pressed()
             if keys[pygame.K_d]:
                 user_player.move_player("right")
+                house_x = house_x - 15
+                tree_x = tree_x - 15
             if keys[pygame.K_a]:
                 user_player.move_player("left")
+                house_x = house_x + 10
+                tree_x = tree_x + 10
+
+        # if house_x < -250:
+        #     house_x = INITIAL_HOUSE_X
+        #
+        # if tree_x < -250:
+        #     tree_x = INITIAL_TREE_X
+
+        screen.blit(bg_1, (0, 0))
+        screen.blit(house, (house_x, 360))
+        screen.blit(tree, (tree_x, 360))
+        screen.blit(user_player.image, user_player.rect)
+        pygame.display.update()
+
+        # while user_player.rect.x < 300:
+        #     screen.blit(bg_1, (0, 0))
+        #     screen.blit(house, (house_x, 360))
+        #     screen.blit(tree, (tree_x, 360))
+        #     screen.blit(user_player.image, user_player.rect)
+        #     pygame.display.update()
+        #
+        #     if user_player.rect.x >= 300:
+        #         bg_1 = bg_2
+        #         size = (800, 600)
+        #         screen = pygame.display.set_mode(size)
+        #         user_player.move_player(direction)
+        #
+        #         screen.blit(bg_2, (0, 0))
+        #         screen.blit(user_player.image, user_player.rect)
+        #         pygame.display.update()
 
         if user_player.rect.x <= 150:
-            house_x = house_x - 2
-            tree_x = tree_x - 2
+
             user_player.move_player(direction)
 
             screen.blit(bg_1, (0, 0))
@@ -99,15 +128,16 @@ while run:
             screen.blit(user_player.image, user_player.rect)
             pygame.display.update()
 
-
-        if user_player.rect.x == 300:
+        if user_player.rect.x >= 300:
+            print('glitch')
             bg_1 = bg_2
-            size = (800, 600)
-            screen = pygame.display.set_mode(size)
+            #size = (800, 600)
+            #screen = pygame.display.set_mode(size)
             user_player.move_player(direction)
 
-            screen.blit(bg_1, (0, 0))
+            screen.blit(bg_2, (0, 0))
             screen.blit(user_player.image, user_player.rect)
+            # screen.blit(player_2, (player_2_x, 360))
             pygame.display.update()
 
     # clock.tick(FPS)
